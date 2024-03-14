9f6e47c46075de03450e5b173a6e110b2d3c403a
Shashwat Shrey <shreyshashwat@gmail.com>
2024-03-14 08:47:26 +0530

    GPTFE2&&MultiLingualFeature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,6 +13,7 @@ const Header = () => {
   const dispatch = useDispatch();
 
   const user = useSelector((store) => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {})
@@ -52,24 +53,28 @@ const Header = () => {
 
   const handleLanguageChange = (e) => {
     dispatch(changeLanguage(e.target.value));
-  }
+  };
 
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img className="w-44" src={LOGO} alt="logo" />
       {user && (
         <div className="flex p-2">
-          <select className="p-2 m-2 bg-gray-900 text-white" onChange={handleLanguageChange}>
-            {SUPPORTED_LANGUAGES.map((lang) => (
-              <option key={lang.identifier} value={lang.identifier}>
-                {lang.name}
-              </option>
-            ))}
-          </select>
+          {showGptSearch && (
+            <select
+              className="p-2 m-2 bg-gray-900 text-white"
+              onChange={handleLanguageChange}>
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
           <button
             className="font-bold py-2 px-4 mx-4 my-2 bg-[#5F4BB6] text-[#F8F7FF] rounded-lg"
             onClick={handleGptSearchClick}>
-            GPT Search
+            {showGptSearch? "Home page" : "GPT Search"}
           </button>
           <img
             className="w-12 h-12 rounded-full"
