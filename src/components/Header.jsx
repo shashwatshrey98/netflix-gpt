8db1a010792116df403639922610e35cef366859
Shashwat Shrey <shreyshashwat@gmail.com>
2024-03-14 07:20:30 +0530

    GPTFE1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,9 @@ import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
-import { LOGO } from "../utils/constants";
+import { LOGO, SUPPORTED_LANGUAGES } from "../utils/constants";
+import { toggleGptSearchView } from "../utils/gptSlice";
+import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -43,18 +45,41 @@ const Header = () => {
     return () => unsubscribe();
   }, []);
 
+  const handleGptSearchClick = () => {
+    // Toggle GPT Search Button
+    dispatch(toggleGptSearchView());
+  };
+
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  }
+
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
-      <img
-        className="w-44"
-        src={LOGO}
-        alt="logo"
-      />
+      <img className="w-44" src={LOGO} alt="logo" />
       {user && (
         <div className="flex p-2">
-          <img className="w-12 h-12" src={user?.photoURL} alt="usericon" />
-          <button onClick={handleSignOut} className="font-bold text-white">
-            (Sign Out)
+          <select className="p-2 m-2 bg-gray-900 text-white" onChange={handleLanguageChange}>
+            {SUPPORTED_LANGUAGES.map((lang) => (
+              <option key={lang.identifier} value={lang.identifier}>
+                {lang.name}
+              </option>
+            ))}
+          </select>
+          <button
+            className="font-bold py-2 px-4 mx-4 my-2 bg-[#5F4BB6] text-[#F8F7FF] rounded-lg"
+            onClick={handleGptSearchClick}>
+            GPT Search
+          </button>
+          <img
+            className="w-12 h-12 rounded-full"
+            src={user?.photoURL}
+            alt="usericon"
+          />
+          <button
+            onClick={handleSignOut}
+            className="font-bold py-2 px-4 mx-4 my-2 bg-[#D62839] text-[#04080F] rounded-lg">
+            Sign Out
           </button>
         </div>
       )}
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import MovieCard from "./MovieCard";
+import "../index.css";
 
 const MovieList = ({ title, movies }) => {
   return (
     <div className="px-6 bg-black">
       <h1 className="text-3xl py- text-white">{title}</h1>
-      <div className="flex overflow-x-scroll">
+      <div className="flex overflow-x-scroll container-snap">
         <div className="flex">
           {movies?.map((movie) => (
             <MovieCard key={movie.id} posterPath={movie.poster_path} />
